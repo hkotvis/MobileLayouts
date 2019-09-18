de4f7af6a2133461c922294b6dcd140b7b181e1e
hkotvis <46495573+hkotvis@users.noreply.github.com>
2019-09-17 20:48:34 -0500

    Layouts lab

---
+++ MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ActivityLayouts
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void navigateGrid(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Grid());
+        }
+        private async void navigateAbsolute(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Absolute());
+        }
+        private async void navigateRelative(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Relative());
+        }
+        private async void navigateFlex(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Flex());
+        }
+    }
+}
